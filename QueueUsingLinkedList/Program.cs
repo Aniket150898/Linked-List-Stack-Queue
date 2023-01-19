1e4e78898b3fc8026439a7a6666e653c49856cad
Aniket150898 <103835629+Aniket150898@users.noreply.github.com>
2023-01-19 21:55:08 +0530

    [Aniket]- Added Program First

---
+++ QueueUsingLinkedList/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace QueueUsingLinkedList
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            LinkedListQueue linkedListQueue = new LinkedListQueue();
+            linkedListQueue.Enqueue(36);
+            linkedListQueue.Enqueue(72);
+            linkedListQueue.Enqueue(82);
+            linkedListQueue.Display();// this.top=this.top.next
+            Console.ReadKey();
+        }
+    }
+}
